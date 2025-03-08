6c2b5e6ba4ce21339b6b0555f043852fa7326052
codersudhir <nandanesudhir1@gmail.com>
2025-03-08 13:22:23 +0530

    push final output

--- Frontend/src/redux/store.ts
+++ Frontend/src/redux/store.ts
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
-import taskReducer from "./taskSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
-    tasks: taskReducer,
   },
 });
 
